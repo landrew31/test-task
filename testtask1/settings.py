39fb5f377b0d74a452e2acb322c360d30989a87c
landrew31 <lupa_andriy@list.ru>
2016-03-25 21:45:12 +0200

    added html

--- testtask1/settings.py
+++ testtask1/settings.py
@@ -109,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
--- testtask1/urls.py
+++ testtask1/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
+from testnews.views.news_list import NewsView
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # News urls
+    url(r'^$', NewsView.as_view(), name='home'),
+
+
+
+    url(r'^admin/', include(admin.site.urls)),
 ]
